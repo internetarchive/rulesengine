b0959366297d5c0064ca2fec3231ffb6a5010719
Barbara Miller <galgeek@me.com>
2021-03-02 14:48:03 -0800

    settings.py updates

--- rulesengine/rulesengine/settings.py
+++ rulesengine/rulesengine/settings.py
@@ -27,6 +27,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
         'localhost',
+        'focal',
+        '0.0.0.0',
         ]
 
 
@@ -47,7 +49,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    #'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
