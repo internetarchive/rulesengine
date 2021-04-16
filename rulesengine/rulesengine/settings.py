9a3cd04a36908ceacb7f76dc89a7c74f61786762
Barbara Miller <barbara@archive.org>
2021-04-16 23:57:00 +0000

    postgres not sqlite

--- rulesengine/rulesengine/settings.py
+++ rulesengine/rulesengine/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'rulesengine.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'rulesengine',
+        'USER': 'rulesengineer',
+        'PASSWORD': 's33krit',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
