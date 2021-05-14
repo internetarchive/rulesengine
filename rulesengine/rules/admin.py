ce40e637652a3fae015525bb56e247918c96ce9a
Barbara Miller <galgeek@me.com>
2021-05-14 20:38:20 +0000

    minor variation on Alex's admin update

--- rulesengine/rules/admin.py
+++ rulesengine/rules/admin.py
@@ -6,5 +6,9 @@ from .models import (
 )
 
 # Register your models here.
-admin.site.register(Rule)
+class RuleAdmin(admin.ModelAdmin):
+    list_display = ("policy", "surt", "collection", "partner", "capture_date_start", "capture_date_end", "enabled")
+    list_filter = ("enabled", "partner", "collection")
+    search_fields = ("surt",)
+admin.site.register(Rule, RuleAdmin)
 admin.site.register(RuleChange)
