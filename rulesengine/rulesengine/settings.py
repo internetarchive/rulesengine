919d818f88c9d20d7a568ec90f2bae603f8eb7ba
Barbara Miller <galgeek@users.noreply.github.com>
2021-08-16 15:56:08 -0700

    tidy settings.py

--- rulesengine/rulesengine/settings.py
+++ rulesengine/rulesengine/settings.py
@@ -27,9 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     'localhost',
-    'focal',
-    '0.0.0.0',
-    'rulesengine-local'
+    '0.0.0.0'
 ]
 
 
@@ -51,7 +49,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    #'django.middleware.csrf.CsrfViewMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
