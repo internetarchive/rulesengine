628f1e613c5d5153546891a07ad830bade7ee284
Barbara Miller <barbara@archive.org>
2021-08-26 18:46:49 +0000

    original db config

--- rulesengine/rulesengine/settings.py
+++ rulesengine/rulesengine/settings.py
@@ -84,7 +84,7 @@ WSGI_APPLICATION = 'rulesengine.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'rulesengine'
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
